e0bcaf1fead8ce1543960d90e740dec644116b30
sabri bargougui <sabri1.bargougui@gmail.com>
2021-01-15 20:17:30 +0100

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID j4HrFGzFmzYFusA5eD9eKcLvx7FfjqsdnJrZ88zU-Dc'
+        Authorization: 'Client-ID <add your client id here >'
     }
 
 });
